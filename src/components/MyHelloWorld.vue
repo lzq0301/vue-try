ab257ff9fdcbeca87155ffa9b6c03730357f4cd4
lzq0301 <lzq0301@126.com>
2018-09-20 14:57:53 +0800

    添加新页

---
+++ src/components/MyHelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <span>hello world!</span>
+        <span>{{msg}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MyHelloWorld',
+  data () {
+    return {
+      msg: '到此一游'
+    }
+  }
+}
+</script>
